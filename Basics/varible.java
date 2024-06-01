26f5af5ef190a822262dcf9b9578012fa6842067
sarrthak008 <sarthaknavale08@gmail.com>
2024-06-01 06:24:27 +0530

    learn about variable

---
+++ Basics/varible.java
@@ -0,0 +1,13 @@
+public class varible {
+    public static void main(String[] args) {
+        int a =10;
+        int b = 20;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Sarthak";
+        System.out.println(name);
+        // change the value of variable 
+        a = 30;
+        System.out.println(a); 
+    }
+}
