d38fecc89c0a6f6c4800dd187182fbbbf01e173e
sarrthak008 <sarthaknavale08@gmail.com>
2024-06-07 12:51:34 +0900

    write first hello world function

---
+++ Function/helloFun.java
@@ -0,0 +1,11 @@
+
+public class helloFun {
+
+    //our hellow created functon thats cant return any thing
+       public static void printHelloWorld(){
+        System.out.println("HEllO WORlD");
+       }
+     public static void main(String[] args) {
+        printHelloWorld();
+     }
+}
