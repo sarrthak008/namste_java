2bb7cecdced2180d9450110a7e7ead41ac5b0f10
sarrthak008 <sarthaknavale08@gmail.com>
2024-06-09 10:26:09 +0900

    add swap.java file

--- Function/swap.java
+++ Function/swap.java
@@ -1,7 +1,33 @@
 public class swap {
    
+    public static void Swap(int A, int B) {
+        int temp;
+        temp = A;
+        A=B;
+        B=temp;
+         System.out.println("A"+A); 
+         System.out.println("B"+B);
+  
+           /* successfull swap the values..
+            
+           but A and B are copy so it cant change in main function..
+           in java every function work on call by values..
+
+           every time we send copy of variable we cant send original value..
+
+           */
+
+    }
+
      public static void main(String[] args) {
-        
+      
+           //swap of numbers
+           int A= 10;
+           int B=20;
+           Swap(A,B);
+
+     
+
      }
      
 }
